01845b624775c90922f811894d4c1cd3fa59c9d3
Michael4Browna <Michael4Browna@gmail.com>
2024-09-04 19:02:37 +0900

    Update home.js

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,12 +1,6 @@
-/*(function () {
-
-})();*/
-
-document.addEventListener("DOMContentLoaded", function() {
-    const searchParams = new URLSearchParams(window.location.search);
-    if (searchParams.has('lptag')) {
-        const newUrl = "https://link.coupang.com/re/AFFSDP?" + searchParams.toString();
-        console.log("리디렉션 중: ", newUrl);
-        window.location.href = newUrl;
-    }
-});
+// 페이지가 로드되기 전에 리디렉션을 처리
+const searchParams = new URLSearchParams(window.location.search);
+if (searchParams.has('lptag')) {
+    const newUrl = "https://link.coupang.com/re/AFFSDP?" + searchParams.toString();
+    window.location.href = newUrl;
+}
