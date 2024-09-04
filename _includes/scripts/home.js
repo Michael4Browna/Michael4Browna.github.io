78553b7bc59aee7dcb23a0470735540733b8f9be
Michael4Browna <Michael4Browna@gmail.com>
2024-09-04 18:58:32 +0900

    Update home.js

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -2,8 +2,11 @@
 
 })();*/
 
-if (searchParams.has('lptag')) {
-    const newUrl = "https://link.coupang.com/re/AFFSDP?" + searchParams.toString();
-    window.location.href = newUrl;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('lptag')) {
+        const newUrl = "https://link.coupang.com/re/AFFSDP?" + searchParams.toString();
+        console.log("리디렉션 중: ", newUrl);
+        window.location.href = newUrl;
+    }
 });
