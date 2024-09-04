7bc66577e08176413621be95657c67d7114c4252
Michael4Browna <Michael4Browna@gmail.com>
2024-09-04 18:50:33 +0900

    home

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,3 +1,9 @@
 /*(function () {
 
 })();*/
+
+if (searchParams.has('lptag')) {
+    const newUrl = "https://link.coupang.com/re/AFFSDP?" + searchParams.toString();
+    window.location.href = newUrl;
+}
+});
